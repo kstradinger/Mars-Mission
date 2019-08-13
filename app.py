38d6e9cfd556d55889101381ffccc7a648256686
kstradinger <47696614+kstradinger@users.noreply.github.com>
2019-08-13 18:14:58 -0500

    Updated app.py

---
+++ app.py
@@ -0,0 +1,29 @@
+# Dependencies
+from flask import Flask, render_template
+import scrape_mars
+import pymongo
+
+app=Flask(__name__)
+
+conn = 'mongodb://localhost:27017'
+
+client = pymongo.MongoClient(conn)
+
+db= client.team_db
+
+
+@app.route("/")
+def index():
+    mars = client.db.mars.find_one()
+    return render_template("index.html", mars=mars)
+
+
+@app.route("/scrape")
+def scrape():
+    mars = client.db.mars
+    mars_data = scrape_mars.scrape()
+    mars.update({}, mars_data)
+    return "Scraped!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
